f7529bfefad54ae08d4ec05e266d760bf4ca5677
Robsen <robsengudeta1@outlook.com>
2025-12-01 12:45:08 -0500

    final

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,9 @@
+
+export default new Map([
+["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/reference/authentication.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Freference%2Fauthentication.mdx&astroContentModuleFlag=true")],
+["src/content/docs/reference/datasets.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Freference%2Fdatasets.mdx&astroContentModuleFlag=true")],
+["src/content/docs/reference/graphs.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Freference%2Fgraphs.mdx&astroContentModuleFlag=true")],
+["src/content/docs/reference/nodesets.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Freference%2Fnodesets.mdx&astroContentModuleFlag=true")],
+["src/content/docs/reference/users.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Freference%2Fusers.mdx&astroContentModuleFlag=true")]]);
+		
